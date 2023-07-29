9b0a9ae10320be3e6f5d43c4128211e1449a239d
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-29 13:00:00 +0100

    first commit

---
+++ calender.py
@@ -0,0 +1,42 @@
+from tkinter import Tk,Canvas
+from datetime import date,datetime
+
+root = Tk()
+c = Canvas(root,width=800,height=800,bg='gray')
+c.pack()
+
+c.create_text(100,50,anchor='w',fill='blue', font='Arial 28 bold underline' ,text="My Countdown Calendar")
+
+def get_events():
+    list_events = []
+    name_events = []
+    with open('events.txt','r') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            current_event = line.split(',')
+            
+            event_date = datetime.strptime(current_event[1], "%d/%m/%Y").date()
+            current_event[1] = event_date
+            
+            list_events.append(current_event)
+            #name_events.append(current_event[0])
+    return list_events
+
+def days_between_dates(date1,date2):
+    time_between = str(date1-date2)
+    number_of_days = time_between.split(' ')
+    return number_of_days[0]
+
+events = get_events()
+today  = date.today()
+vertical_space = 100
+for event in events:
+    event_name = event[0]
+    
+    days_until = days_between_dates(event[1],today)
+    display = f"It is {days_until} days until {event_name}"
+
+    c.create_text(100,vertical_space,anchor='w',fill='orange', \
+                  font='Arial 28 bold' ,text=display)
+    vertical_space = vertical_space + 40
+root.mainloop()
