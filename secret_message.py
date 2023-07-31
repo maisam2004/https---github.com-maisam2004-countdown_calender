72ff528a1317cb7dfe28d77fcf03d93524ef1779
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-31 12:31:02 +0100

    Create func to enter secret message

--- secret_message.py
+++ secret_message.py
@@ -3,3 +3,7 @@ from tkinter import Tk,messagebox,simpledialog
 def get_task():
     task = simpledialog.askstring('Task','Do you want to encrypt or decrypt ? ')
     return task
+
+def get_message():
+    message = simpledialog.askstring('Message',"Enter the secret message: ")
+    return message
