2add8685539695c2d6d5f387e7fcc7e646e9a12f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-31 12:42:04 +0100

    Create while loop fo ask for task and message

--- secret_message.py
+++ secret_message.py
@@ -1,9 +1,22 @@
 from tkinter import Tk,messagebox,simpledialog
 
 def get_task():
-    task = simpledialog.askstring('Task','Do you want to encrypt or decrypt ? ')
-    return task
+    return simpledialog.askstring('Task','Do you want to encrypt or decrypt ? ')
 
 def get_message():
-    message = simpledialog.askstring('Message',"Enter the secret message: ")
-    return message
+    return simpledialog.askstring('Message',"Enter the secret message: ")
+
+root = Tk
+
+while True:
+    task = get_task()
+
+    if task == 'encrypt':
+        message =get_message()
+        messagebox.showinfo('Message to encrypt is : ',message)
+    elif task == 'decrypt':
+        message = get_message()
+        messagebox.showinfo('Message to decrypt is :',message)
+    else:
+        break
+root.mainloop()
