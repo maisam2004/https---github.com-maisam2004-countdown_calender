28eec70e230d9270be06f6d59042103a4f47dd9b
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-01 12:30:41 +0100

    Add iseven and get_even_letters, odd letter metods

--- secret_class.py
+++ secret_class.py
@@ -5,4 +5,13 @@ class Secret_message:
     ROOT.withdraw()
     def __init__(self):
         self.message = simpledialog.askstring('Message',"Enter the secret message: ")
-        
+        self.task = simpledialog.askstring('Encrypt or Decrypt', 'Do you want to encrypt or decrypt?')
+
+    def is_even(self,num):
+        return num % 2 == 0
+
+    def get_even_letters(self,message): #produces a list containing all the even-numbered letters.
+        return [message[counter] for counter in range(len(message)) if is_even(counter)]
+
+    def get_odd_letters(self,message): #produces a list containing all the even-numbered letters.
+        return [message[counter] for counter in range(len(message)) if not is_even(counter)]    
