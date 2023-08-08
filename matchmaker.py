f9805fde6a4e19b5594430d2cff02d0e9e96dc0e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-08-08 12:14:40 +0100

    Add variables and symbols codes

--- matchmaker.py
+++ matchmaker.py
@@ -1,4 +1,4 @@
-import random
+from random import shuffle 
 import time
 from tkinter import Tk,Button,DISABLED
 
@@ -6,4 +6,15 @@ root =Tk()
 root.title("Matchmaker")
 root.resizable(width=False,height=False)
 
+root.mainloop()
+buttons = {}
+first = True #check symbol is the first in math
+previousX = 0 #track last button pressed
+previousY = 0
 
+button_symbol = {}
+
+symbols = [u'\u2702', u'\u2702', u'\u2705', u'\u2705', u'\u2708', u'\u2708',
+u'\u2709', u'\u2709', u'\u270A', u'\u270A', u'\u270B', u'\u270B',
+u'\u270C', u'\u270C', u'\u270F', u'\u270F', u'\u2712', u'\u2712',
+u'\u2714', u'\u2714', u'\u2716', u'\u2716', u'\u2728', u'\u2728']
